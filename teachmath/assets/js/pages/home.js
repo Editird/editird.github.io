944165739c08d1fb80a66f0650100bdc850285ea
Editird <166622722+Editird@users.noreply.github.com>
2026-07-07 06:45:59 +0000

    remove math teach and web lvl c

--- teachmath/assets/js/pages/home.js
+++
@@ -1,8 +0,0 @@
-import { homeShowcaseLesson } from "../content/guidedLessons.js";
-import { renderSolveTheater } from "../lib/solveTheater.js";
-import { setupLayout } from "../main.js";
-setupLayout();
-const homeTheater = document.querySelector("[data-home-theater]");
-if (homeTheater) {
-    renderSolveTheater(homeTheater, homeShowcaseLesson);
-}
